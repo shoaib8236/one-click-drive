01dade93186e066ba74976661a37a3b3eb26acc6
shoaib8236 <shoaib.raza.dev@gmail.com>
2023-07-20 12:04:49 -0700

    404 page

---
+++ app/item-of-use/page.js
@@ -0,0 +1,9 @@
+
+
+const ItemOfUse = ()=> {
+    return (
+        <h1>Item Of Use</h1>
+    )
+}
+
+export  default  ItemOfUse
---
+++ app/terms-and-condition/page.js
@@ -0,0 +1,8 @@
+
+const TermsAndCondition = ()=> {
+    return (
+        <h1>Terms And Condition</h1>
+    )
+}
+
+export  default  TermsAndCondition
