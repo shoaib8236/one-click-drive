5ed41dd5ae7cfb08200d80c2ce5edae63967f999
shoaib8236 <shoaib.raza.dev@gmail.com>
2023-07-20 07:50:43 -0700

    New page added

--- app/car-brand/[type]/page.js
+++ app/car-brand/[type]/page.js
@@ -3,12 +3,8 @@ const Banner = dynamic(() => import('@/components/Banner/Banner'));
 const Section = dynamic(() => import('@/components/Section/Section'));
 const Page = ({ params }) => {
     return (
-        <div>
-            <Banner
-                image="/home-banner.jpg"
-                title={params.type}
-                subtitle="Book directly from local suppliers. No commission, no mark-ups."
-            />
+        <div className="car_brands_page_wrapper">
+            
             <Section></Section>
         </div>
     );
---
+++ components/StyledCollapsed/StyledCollapsed.js
@@ -0,0 +1,18 @@
+'use client'
+import React, {useState} from "react";
+
+
+const StyledCollapsed = (props) => {
+    const {itemShow = 8} = props;
+
+    const [items, setItems] = useState([])
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default  StyledCollapsed
